5f4c61dd2352089476e9fae267fd20cf578ab9ee
KaNa <47849640+J-Kana@users.noreply.github.com>
2022-09-28 15:17:57 +0600

    Delete smsConsultCreds.js

--- smsConfigs/smsConsultCreds.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    //sms_login: 'mlombard',
-    //sms_password: '16gK9rqw',
-    //sms_sender: 'M-Lombard'
-    sms_login: 'M-Lombard_MAX',
-    sms_password: '9uq68Kta',
-    sms_sender: 'M-Lombard'
-};
