8811ab67f0eadd6f91bdf54e2490860499176542
KaNa <47849640+J-Kana@users.noreply.github.com>
2022-09-28 15:17:37 +0600

    Delete http.js

--- smsConfigs/http.js
+++
@@ -1,7 +0,0 @@
-var {domain} = require('./environment');
-
-
-module.exports = {
-  "I18N_SERVICE_URL": domain+"i18n/dynamicMessages/getMessageWithParams",
-  "invalidateUserSessionData": domain+"auth/invalidateUserSessionData"
-};
